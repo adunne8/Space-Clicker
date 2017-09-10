0ebf78e508690132d3e0e10831dcc53db36f72eb
adunne8 <adunne08@gmail.com>
2017-09-10 18:27:48 +0100

    added responsive code and worker addition

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,7 @@
 var food = 0;
 var wood = 0;
 var stone = 0;
+var worker = 0;
 
 var clickCount = 0;
 
@@ -10,6 +11,8 @@ window.onload = function() {
   var foodButton = document.getElementById('food');
   var woodButton = document.getElementById('wood');
   var stoneButton = document.getElementById('stone');
+
+  var spawnButton = document.getElementById('worker');
 
   foodButton.addEventListener("click", function() {
     increment(this.id);
@@ -21,15 +24,26 @@ window.onload = function() {
     increment(this.id);
   });
 
+  spawnButton.addEventListener("click", function() {
+    spawn(this.id);
+  });
+
   function increment(resource) {
     clickCount++;
 
     window[resource]++;
     document.getElementById(resource + "_count").innerHTML = window[resource];
+  }
 
-    console.log(resource);
-    console.log(food);
-    console.log(resource + clickCount);
+  function spawn(resource){
+    clickCount++;
+
+    if(food >= 10){
+      window[resource]++;
+      document.getElementById(resource + "_count").innerHTML = window[resource];
+      food = food -10;
+      document.getElementById("food_count").innerHTML = food;
+    }
 
   }
 
